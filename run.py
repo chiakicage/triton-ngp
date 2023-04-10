5bfcd3d4b654875574190f9864f66b7db94f800d
chiakicage <colorful_cache@outlook.com>
2023-04-11 01:22:28 +0800

    added config

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 from omegaconf import DictConfig, OmegaConf
+import hydra
+
+@hydra.main(version_base=None, config_path="config", config_name="config")
+def main(cfg: DictConfig):
+	print(OmegaConf.to_yaml(cfg))
+
+if __name__ == "__main__":
+	main()
